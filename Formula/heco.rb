a62bd39b9607425ea7d36fc8198dda3dec3ff46a
Ockham Liu <ockhamliu@outlook.com>
2026-04-22 16:52:59 +0000

    chore(release): bump version to 0.0.1 in Formula/heco.rb

--- Formula/heco.rb
+++ Formula/heco.rb
@@ -9,10 +9,10 @@ class Heco < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/heco-cli/heco/releases/download/v#{version}/heco-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_MAC_INTEL_SHA256" # MAC_INTEL
+      sha256 "bf814c397cbc646dc2778e9cf1fb34f08b165f732066e01814431c38610856b8" # MAC_INTEL
     elsif Hardware::CPU.arm?
       url "https://github.com/heco-cli/heco/releases/download/v#{version}/heco-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_MAC_ARM_SHA256" # MAC_ARM
+      sha256 "11bfc984bb1788d7731556aa7145de0386408dc3d86f8cf1de506697fac25246" # MAC_ARM
     end
   end
 
